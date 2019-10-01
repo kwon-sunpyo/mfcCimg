b9a285a4a8ab02c589d7d14a869be778c13715da
kwon-sunpyo <54826097+kwon-sunpyo@users.noreply.github.com>
2019-10-01 11:20:00 +0900

    Update imgDlg.cpp

--- img/imgDlg.cpp
+++ img/imgDlg.cpp
@@ -164,6 +164,23 @@ HCURSOR CimgDlg::OnQueryDragIcon()
 
 void CimgDlg::OnBnClickedOk()
 {
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-	CDialogEx::OnOK();
+	int i, j, hig, wid, stride, n;
+	hig = img.GetHeight();
+	wid = img.GetWidth();
+	stride = img.GetPitch();
+
+	BYTE* pBits = (BYTE*)img.GetBits();
+	BYTE* pRow;
+	n = img.GetBPP() / 8; // How many pixels
+	TRACE("bpp=%d", n);
+	for (j = 0; j < hig; j++) {
+		pRow = pBits + j * stride;
+		for (i = 0; i < wid * n; i += n) {
+			pRow[i + 0] = 255 - pRow[i + 0]; //R
+			pRow[i + 1] = 255 - pRow[i + 1]; //G
+			pRow[i + 2] = 255 - pRow[i + 2]; //B
+			// pRow[i+3] = pRow[i+2];  // Transparent
+		}
+	}
+	Invalidate(true);
 }
